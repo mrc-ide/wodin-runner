8830e37a692aa63ab0130633e8a8d18a9ea1b1aa
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-06-15 13:02:52 +0100

    Fix lint

--- src/maths.ts
+++ src/maths.ts
@@ -10,14 +10,14 @@ export function round2(x: number, digits?: number) {
     if (digits === undefined || digits === 0) {
         return roundHalfToEven(x);
     } else {
-        var mult = Math.pow(10, digits);
+        const mult = Math.pow(10, digits);
         return roundHalfToEven(x * mult) / mult;
     }
 }
 
 // modulo that conforms to (approximately) the same behaviour as R
 export function modr(x: number, y: number) {
-    var tmp = x % y;
+    let tmp = x % y;
     if (tmp * y < 0) {
         tmp += y;
     }
