56dcc229729a7f31f2ddfd5ae07537612b61c0bd
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-07-13 17:23:43 +0100

    Save version information into package

---
+++ scripts/versions.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const fs = require('fs');
+
+const root = path.resolve(__dirname, "..");
+const lock = JSON.parse(fs.readFileSync(path.join(root, "package-lock.json")));
+const dest = path.join(root, "src", "versions.ts")
+
+var code = ["export function versions() {",
+              "    return {",
+              `        "dfoptim": "${lock.packages["node_modules/dfoptim"].version}",`,
+              `        "dopri": "${lock.packages["node_modules/dopri"].version}",`,
+              `        "odin": "${lock.version}",`,
+              "    };",
+              "}"];
+
+fs.writeFileSync(dest, code.map((x) => x + "\n").join(""));
---
+++ src/versions.ts
@@ -0,0 +1,7 @@
+export function versions() {
+    return {
+        "dfoptim": "0.0.5",
+        "dopri": "0.0.12",
+        "odin": "0.0.4",
+    };
+}
