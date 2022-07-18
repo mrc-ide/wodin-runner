a822234428970f99eb443c7dc92ea3671564e44e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-07-18 12:50:31 +0100

    Add basic interpolation wrappers
    Port of previous js code

---
+++ src/interpolate.ts
@@ -0,0 +1,102 @@
+import {
+    InterpolatorBase,
+    InterpolatorConstant,
+    InterpolatorLinear,
+    InterpolatorSpline,
+} from "@reside-ic/interpolate";
+
+/**
+ * Information about interpolation bounds
+ */
+interface InterpolateTimes {
+    /** The minimum time supported by the interpolation functions */
+    max: number;
+    /** The maximum time supported by the interpolation functions */
+    min: number;
+}
+
+/** Allocate a new interpolation function object
+ *
+ * @param method Interpolation method to use; one of `constant`,
+ * `linear` or `spline`
+ *
+ * @param t Array of interpolation times
+ *
+ * @param y Array of interpolation response variables
+ */
+export function interpolateAlloc(method: string,
+                                 t: number[], y: number[]): InterpolatorBase {
+    if (method === "constant") {
+        return new InterpolatorConstant(t, y);
+    } else if (method === "linear") {
+        return new InterpolatorLinear(t, y);
+    } else if (method === "spline") {
+        return new InterpolatorSpline(t, y);
+    } else {
+        throw Error(`Invalid interpolation method '${method}'`);
+    }
+}
+
+/** Check that interpolation values are reasonable; this is called
+ * before allocation, once per model initialisation or parameter
+ * setting
+ */
+export function interpolateCheckY(dimArg: number[], dimTarget: number[],
+                                  nameArg: string, nameTarget: string) {
+    if (dimTarget.length === 1) {
+        if (dimArg[0] !== dimTarget[0]) {
+            throw Error(`Expected ${nameArg} to have length ${dimArg[0]}` +
+                        ` (for ${nameTarget})`);
+        }
+    } else {
+        for (let i = 0; i < dimTarget.length; ++i) {
+            if (dimArg[i] !== dimTarget[i]) {
+                throw Error(`Expected dimension ${i + 1} of ${nameArg}` +
+                            ` to have size ${dimArg[i]} (for ${nameTarget})`);
+            }
+        }
+    }
+}
+
+/** Check that the integration times are compatible with the
+ * interpolation functions (i.e., that they would not cause
+ * extrapolation). If the model does not have
+ *
+ * @param tStart Start time of the integration
+ *
+ * @param tEnd End time of the integration
+ *
+ * @param times Information about the interpolation bounds
+ */
+export function interpolateCheckT(tStart: number, tEnd: number,
+                                  times?: InterpolateTimes) {
+    if (times === undefined) {
+        return Infinity;
+    }
+    if (tStart < times.min) {
+        throw Error("Integration times do not span interpolation range;" +
+                    ` min: ${times.min}`);
+    }
+    if (tEnd > times.max) {
+        throw Error("Integration times do not span interpolation range;" +
+                    ` max: ${times.max}`);
+    }
+    // This logic needs tweaking once we expose the critical time
+    // interface more broadly (i.e., where any model can add a
+    // critical time; mrc-3418)
+    return times.max;
+}
+
+export function interpolateTimes(start: number[], end: number[]): InterpolateTimes {
+    // min time would be the latest (max) interpolation start
+    const min = Math.max(...start);
+    // max time would be the earliest (min) interpolation end
+    const max = Math.min(...end);
+    return {max, min};
+}
+
+export const interpolate = {
+    alloc: interpolateAlloc,
+    checkY: interpolateCheckY,
+    times: interpolateTimes,
+};
---
+++ test/interpolate.test.ts
@@ -0,0 +1,74 @@
+import {interpolateAlloc, interpolateCheckT, interpolateCheckY, interpolateTimes} from "../src/interpolate";
+
+describe("Can validate interpolation argumements", () => {
+    it("accepts valid inputs", () => {
+        interpolateCheckY([10], [10], "x", "y");
+        interpolateCheckY([10, 2, 5], [10, 2, 5], "x", "y");
+    });
+
+    it("rejects incorrectly sized vector inputs", () => {
+        expect(() => interpolateCheckY([10], [9], "x", "y")).
+            toThrow("Expected x to have length 10 (for y)");
+        expect(() => interpolateCheckY([10, 2, 5], [9, 2, 5], "x", "y")).
+            toThrow("Expected dimension 1 of x to have size 10 (for y)");
+    });
+});
+
+describe("Can validate interpolation times", () => {
+    it("Requires no tcrit with no interpolationTimes", () => {
+        expect(interpolateCheckT(0, 10)).toBe(Infinity);
+        expect(interpolateCheckT(0, 10, undefined)).toBe(Infinity);
+    });
+
+    it("Requires no tcrit with no max interpolation time", () => {
+        const t = {min: 0, max: Infinity};
+        expect(interpolateCheckT(0, 10, t)).toBe(Infinity);
+    });
+
+    it("Can report back tcrit from interpolation times", () => {
+        const t = {min: 0, max: 11};
+        expect(interpolateCheckT(0, 10, t)).toBe(11);
+    });
+
+    it("Requires that integration times do not extrapolate", () => {
+        const t = {min: 0, max: 10};
+        expect(() => interpolateCheckT(-5, 10, t))
+            .toThrow("Integration times do not span interpolation range; min: 0");
+        expect(() => interpolateCheckT(0, 20, t))
+            .toThrow("Integration times do not span interpolation range; max: 10");
+    });
+
+    it("constructs sensible interpolation times", () => {
+        expect(interpolateTimes([], [])).toEqual({min: -Infinity, max: Infinity});
+        expect(interpolateTimes([3, 2, 1], [])).toEqual({min: 3, max: Infinity});
+        expect(interpolateTimes([3, 2, 1], [5, 6, 7])).toEqual({min: 3, max: 5});
+    });
+});
+
+describe("Can construct interpolation objects", () => {
+    const t = [0, 1, 2, 3, 4, 5, 6];
+    const y = t.map((x: number) => x * x);
+
+    it("constructs constant interpolators", () => {
+        const obj = interpolateAlloc("constant", t, y);
+        expect(obj.eval(3, 0)).toEqual(9);
+        expect(obj.eval(3.5, 0)).toEqual(9);
+    });
+
+    it("constructs linear interpolators", () => {
+        const obj = interpolateAlloc("linear", t, y);
+        expect(obj.eval(3, 0)).toEqual(9);
+        expect(obj.eval(3.5, 0)).toEqual(12.5);
+    });
+
+    it("constructs spline interpolators", () => {
+        const obj = interpolateAlloc("spline", t, y);
+        expect(obj.eval(3, 0)).toEqual(9);
+        expect(obj.eval(3.5, 0)).toBeCloseTo(12.2548077);
+    });
+
+    it("refuses to construct unknown interpolators", () => {
+        expect(() => interpolateAlloc("other", t, y))
+            .toThrow("Invalid interpolation method 'other'");
+    });
+});
